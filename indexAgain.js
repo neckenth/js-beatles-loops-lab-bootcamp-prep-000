3169138f5833e7da461deccb7d2691c14bc143c4
Nancy Eckenthal <nancy.eckenthal@gmail.com>
2017-12-01 18:16:33 +0000

    automatically backed up by learn

--- indexAgain.js
+++ indexAgain.js
@@ -1,7 +1,7 @@
 function thBeatlesPlay(m, i) {
     var a = []
     for (var n = 0; n < m.length; n++) {
-        a = [...a, `"${m[n]} plays ${i[n]}"`]
+        a = a.push(`"${m[n]} plays ${i[n]}"`)
     }
     return a
 }
